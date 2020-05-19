540117abab66a82f8beffa70e619e13b727dec17
Malibin <nightmare73@naver.com>
2020-05-19 22:48:03 +0900

    일단세이브

--- app/src/main/java/com/malibin/acnh/wiki/MainActivity.kt
+++ app/src/main/java/com/malibin/acnh/wiki/MainActivity.kt
@@ -2,6 +2,11 @@ package com.malibin.acnh.wiki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 import java.io.FileOutputStream
@@ -18,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseStorage.getInstance()
             .getReference()
-            .child("bug/detail")
+            .child("villager/exterior")
             .listAll()
             .addOnSuccessListener {
                 val totalSize = it.items.size
@@ -26,7 +31,7 @@ class MainActivity : AppCompatActivity() {
                     item.downloadUrl.addOnSuccessListener {
                         urls.add(it.toString())
                         if (urls.size == totalSize) {
-                            saveTextFile(urls)
+                            saveTextFileWithIndex(urls)
                         }
                     }
                 }
@@ -35,14 +40,47 @@ class MainActivity : AppCompatActivity() {
                 println("Failed")
             }
 
+//        val liveData1 = MutableLiveData<Int>()
+//        val liveData2 = MutableLiveData<Int>()
+//
+//        val result = MediatorLiveData<Int>()
+//
+//        result.addSource(liveData1) {
+//            result.setValue(it)
+//        }
+//        result.addSource(liveData2) {
+//            result.value = it
+//        }
+//
+//        result.observe(this, Observer {
+//            Log.d("Malibin Debug", "Observing.... $it")
+//        })
+//
+//        val worker1 = Thread {
+//            for (i in 1..5) {
+//                liveData1.value = i
+//                Thread.sleep(1_000)
+//            }
+//        }
+//        Handler().post(worker1)
+//
+//        val worker2 = Thread {
+//            for (i in 10 downTo 6) {
+//                liveData2.value = i
+//                Thread.sleep(500)
+//            }
+//        }
+//        Handler().post(worker2)
+
+
     }
 
     fun saveTextFileWithIndex(urls: List<String>) {
-        val regex = "villager_poster_(.*)_"
+        val regex = "exterior_(.*)_"
         val pattern = Pattern.compile(regex)
 
         println(getExternalFilesDir(null)?.absolutePath)
-        val file = File(getExternalFilesDir(null), "villager_poster_urls.txt")
+        val file = File(getExternalFilesDir(null), "villager_exterior_urls.txt")
         val stream = FileOutputStream(file)
         val stringBuilder = StringBuilder()
         for (url in urls.sorted()) {
@@ -60,18 +98,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun saveTextFileWithStringIndex(urls: List<String>) {
-        val regex = "_(.*).png"
+        val regex = "reaction_(.*).png"
         val pattern = Pattern.compile(regex)
 //t.com/o/fish%2Ficon%2Fic_fish_002_palechub.png?alt=media&token=8a8436b8-5f59-4fa7-b641-c4017d49058b
         println(getExternalFilesDir(null)?.absolutePath)
-        val file = File(getExternalFilesDir(null), "fish_detail_urls.txt")
+        val file = File(getExternalFilesDir(null), "reaction_urls.txt")
         val stream = FileOutputStream(file)
         val stringBuilder = StringBuilder()
         for (url in urls.sorted()) {
             val matcher = pattern.matcher(url)
             var stringIndex: String? = null
             if (matcher.find()) {
-                stringIndex = matcher.group(1).split("_")[2]
+                stringIndex = matcher.group(1)
             }
             stringBuilder.append(stringIndex).append(",").append(url).append("\n")
         }
