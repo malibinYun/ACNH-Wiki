9da0ae7fe707c28a0f9a0dc78404908c4ae7efaa
Malibin <nightmare73@naver.com>
2020-05-24 23:56:09 +0900

    [add] Villager Data Source Interface 작성

---
+++ app/src/main/java/com/malibin/acnh/wiki/data/source/VillagerDataSource.kt
@@ -0,0 +1,25 @@
+package com.malibin.acnh.wiki.data.source
+
+import com.malibin.acnh.wiki.data.entity.Villager
+
+interface VillagerDataSource {
+
+    suspend fun getAllVillagers(): List<Villager>
+
+    suspend fun getVillagerById(amiiboIndex: Int): Villager
+
+    suspend fun getVillagerInHome(): List<Villager>
+
+    suspend fun getFavoriteVillagers(): List<Villager>
+
+    suspend fun deleteAllVillagers()
+
+    suspend fun checkFavoriteVillager(villager: Villager)
+
+    suspend fun unCheckFavoriteVillager(villager: Villager)
+
+    suspend fun checkHomeVillager(villager: Villager)
+
+    suspend fun unCheckHomeVillager(villager: Villager)
+
+}
