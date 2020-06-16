1a545be474a377dde4295d3ab64c3b3f0d0cdcc5
Malibin <nightmare73@naver.com>
2020-06-16 16:24:16 +0900

    [add] Furniture Dao, LocalSource 작성

---
+++ app/src/main/java/com/malibin/acnh/wiki/data/dao/FurnitureDao.kt
@@ -0,0 +1,50 @@
+package com.malibin.acnh.wiki.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.malibin.acnh.wiki.data.ItemType
+import com.malibin.acnh.wiki.data.entity.Furniture
+
+/**
+ * Created By Malibin
+ * on 6월 16, 2020
+ */
+
+@Dao
+interface FurnitureDao {
+
+    @Query("SELECT * FROM furniture")
+    suspend fun getAllFurniture(): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE itemType = :itemType")
+    suspend fun getFurnitureListOf(itemType: ItemType): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE id IN (:specificIds)")
+    suspend fun getFurnitureListOf(specificIds: List<Int>): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE collected = 1")
+    suspend fun getCollectedFurnitureList(): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE collected = 1 AND itemType = :itemType")
+    suspend fun getCollectedFurnitureListOf(itemType: ItemType): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE wished = 1")
+    suspend fun getWishedFurnitureList(): List<Furniture>
+
+    @Query("SELECT * FROM furniture WHERE wished = 1 AND itemType = :itemType")
+    suspend fun getWishedFurnitureListOf(itemType: ItemType): List<Furniture>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFurniture(items: List<Furniture>)
+
+    @Query("DELETE FROM furniture")
+    suspend fun deleteAllFurniture()
+
+    @Query("UPDATE furniture SET collected = :isChecked WHERE id = :itemId")
+    suspend fun updateFurnitureCollected(itemId: Int, isChecked: Boolean)
+
+    @Query("UPDATE furniture SET wished = :isChecked WHERE id = :itemId")
+    suspend fun updateFurnitureWished(itemId: Int, isChecked: Boolean)
+}
---
+++ app/src/main/java/com/malibin/acnh/wiki/data/source/local/FurnitureLocalDataSource.kt
@@ -0,0 +1,60 @@
+package com.malibin.acnh.wiki.data.source.local
+
+import com.malibin.acnh.wiki.data.ItemType
+import com.malibin.acnh.wiki.data.dao.FurnitureDao
+import com.malibin.acnh.wiki.data.entity.Furniture
+import com.malibin.acnh.wiki.data.source.ItemDataSource
+
+/**
+ * Created By Malibin
+ * on 6월 16, 2020
+ */
+
+class FurnitureLocalDataSource(
+    private val furnitureDao: FurnitureDao
+) : ItemDataSource<Furniture> {
+
+    override suspend fun getAllItems(): List<Furniture> {
+        return furnitureDao.getAllFurniture()
+    }
+
+    override suspend fun getItemsOf(itemType: ItemType): List<Furniture> {
+        return furnitureDao.getFurnitureListOf(itemType)
+    }
+
+    override suspend fun getItemsOf(specificIds: List<Int>): List<Furniture> {
+        return furnitureDao.getFurnitureListOf(specificIds)
+    }
+
+    override suspend fun getCollectedItemsOf(itemType: ItemType): List<Furniture> {
+        return furnitureDao.getCollectedFurnitureListOf(itemType)
+    }
+
+    override suspend fun getCollectedItems(): List<Furniture> {
+        return furnitureDao.getCollectedFurnitureList()
+    }
+
+    override suspend fun getWishedItemsOf(itemType: ItemType): List<Furniture> {
+        return furnitureDao.getWishedFurnitureListOf(itemType)
+    }
+
+    override suspend fun getWishedItems(): List<Furniture> {
+        return furnitureDao.getWishedFurnitureList()
+    }
+
+    override suspend fun saveItems(itemList: List<Furniture>) {
+        furnitureDao.insertFurniture(itemList)
+    }
+
+    override suspend fun deleteAllItems() {
+        furnitureDao.deleteAllFurniture()
+    }
+
+    override suspend fun checkCollected(item: Furniture, isChecked: Boolean) {
+        furnitureDao.updateFurnitureCollected(item.id, isChecked)
+    }
+
+    override suspend fun checkWished(item: Furniture, isChecked: Boolean) {
+        furnitureDao.updateFurnitureWished(item.id, isChecked)
+    }
+}
