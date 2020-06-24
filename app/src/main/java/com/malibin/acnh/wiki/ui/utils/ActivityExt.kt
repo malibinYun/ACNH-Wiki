de8c3a9f07e60f618f0a13163cc9b94394850359
Malibin <nightmare73@naver.com>
2020-06-24 14:20:48 +0900

    [add] StatusBar 색상 변경
    1. 기본으로 grayEF 컬러. 나머지는 그때그때 바꾸는걸로.
    2. StatusBar Color 바꾸는 확장함수 추가

--- app/src/main/java/com/malibin/acnh/wiki/ui/utils/ActivityExt.kt
+++ app/src/main/java/com/malibin/acnh/wiki/ui/utils/ActivityExt.kt
@@ -1,6 +1,10 @@
 package com.malibin.acnh.wiki.ui.utils
 
+import android.os.Build
+import android.view.Window
+import androidx.annotation.ColorRes
 import androidx.annotation.IdRes
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
@@ -24,4 +28,12 @@ fun AppCompatActivity.addFragmentToActivity(
         .add(containerId, fragment, tag)
         .addToBackStack(fragment::class.simpleName)
         .commit()
+}
+
+fun AppCompatActivity.changeStatusBarColor(@ColorRes color: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        this.window.statusBarColor = resources.getColor(color, null)
+    } else {
+        this.window.statusBarColor = resources.getColor(color)
+    }
 }
