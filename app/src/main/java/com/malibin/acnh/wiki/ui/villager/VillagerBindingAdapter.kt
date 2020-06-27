efa5f2f7775ea20a1e1352610b884c712cbd3e33
Malibin <nightmare73@naver.com>
2020-06-27 15:34:13 +0900

    [add] 주민 컬러 띄우기 작성
    1. RecyclerView로 설계하려 했으나 2개 때문에..? 라는 생각에 그냥 BindingAdapter 2개 만드는걸로 구현.
    2. 아직 컬러팔레트가 안넘어와서 tint물들이는걸로 대체
    3. 원래는 컬러풀의 존재떄문에 drawable로 해야될듯, 그리고 테두리있는놈땜에

--- app/src/main/java/com/malibin/acnh/wiki/ui/villager/VillagerBindingAdapter.kt
+++ app/src/main/java/com/malibin/acnh/wiki/ui/villager/VillagerBindingAdapter.kt
@@ -1,13 +1,18 @@
 package com.malibin.acnh.wiki.ui.villager
 
+import android.os.Build
 import android.text.TextUtils
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.malibin.acnh.wiki.data.FavoriteColor
 import com.malibin.acnh.wiki.data.entity.Villager
 import com.malibin.acnh.wiki.ui.utils.toDp
 import com.malibin.acnh.wiki.ui.utils.toPx
+import io.opencensus.resource.Resource
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -43,4 +48,30 @@ fun bindingAmiiboCard(imageView: ImageView, villager: Villager?) {
     Glide.with(imageView)
         .load(villager.amiiboCardUrl)
         .into(imageView)
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+@BindingAdapter("favoriteColorFirst")
+fun bindingFavoriteColorFirst(imageView: ImageView, favoriteColors: List<FavoriteColor>) {
+//    imageView.setBackgroundResource(favoriteColors[0].colorRes)
+    imageView.setColorFilter(
+        imageView.context.resources.getColor(favoriteColors[0].colorRes, null),
+        android.graphics.PorterDuff.Mode.MULTIPLY
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+@BindingAdapter("favoriteColorSecond")
+fun bindingFavoriteColorSecond(imageView: ImageView, favoriteColors: List<FavoriteColor>) {
+    try {
+//        imageView.setBackgroundResource(favoriteColors[1].colorRes)
+        imageView.setColorFilter(
+            imageView.context.resources.getColor(favoriteColors[1].colorRes, null),
+            android.graphics.PorterDuff.Mode.MULTIPLY
+        )
+
+    } catch (e: Exception) {
+        imageView.visibility = View.GONE
+    }
+
 }
